4bf183583d8b14a593132e3d5d43e8118e863177
alexorzechowski <alexander.orzechowski@mail.mcgill.ca>
2018-04-11 17:11:28 -0400

    Changed mem_stage_tb to work with cache
    Changes will also have to be copied over to testbench.vhd

--- mem_stage_tb.vhd
+++ mem_stage_tb.vhd
@@ -22,12 +22,12 @@ port(
 		wb_data : out std_logic_vector(31 downto 0);
 		wb_dest_reg : out std_logic_vector(31 downto 0);
 
-		--data memory communication
+		--cache memory communication
 		mem_read_data : in std_logic_vector (31 downto 0);
 		mem_waitrequest : in std_logic;
 		mem_write : out std_logic;
 		mem_read : out std_logic;
-		mem_addr : out integer RANGE 0 TO 8191;
+		mem_addr : out std_logic_vector(31 downto 0);
 		mem_write_data : out std_logic_vector (31 downto 0);
 
 		--memory stall
@@ -35,22 +35,46 @@ port(
 );
 end component;
 
-component data_memory IS
+component cache IS
+	GENERIC(
+		ram_size : INTEGER := 32768
+	);
+	PORT (
+		clock: IN STD_LOGIC;
+		reset: IN STD_LOGIC;
+
+		s_address: in std_logic_vector (31 downto 0);
+		s_memread: in std_logic;	
+		s_readdata: out std_logic_vector (31 downto 0);
+		s_memwrite: in std_logic;
+		s_writedata: in std_logic_vector (31 downto 0);
+		s_waitrequest: out std_logic; 
+		
+
+		m_address: in integer RANGE 0 TO 8191;
+		m_memread: in std_logic;	
+		m_readdata: out std_logic_vector (31 downto 0);
+		m_memwrite: in std_logic;
+		m_writedata: in std_logic_vector (31 downto 0);
+		m_waitrequest: out std_logic
+	);
+end component;
+component memory IS
 	GENERIC(
 		ram_size : INTEGER := 32768;
+		mem_delay : time := 10 ns;
 		clock_period : time := 1 ns
 	);
 	PORT (
 		clock: IN STD_LOGIC;
 		writedata: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
-		address: IN INTEGER RANGE 0 TO 8191;
+		address: IN INTEGER RANGE 0 TO ram_size-1;
 		memwrite: IN STD_LOGIC;
 		memread: IN STD_LOGIC;
 		readdata: OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
 		waitrequest: OUT STD_LOGIC
 	);
 end component;
-
 signal reset : std_logic;
 signal clk : std_logic;
 
@@ -65,12 +89,22 @@ signal wb_data : std_logic_vector(31 downto 0);
 signal wb_dest_reg : std_logic_vector(31 downto 0);
 
 --data memory communication
+
+signal cache_read_data : std_logic_vector (31 downto 0);
+signal cache_waitrequest : std_logic;
+signal cache_write : std_logic;
+signal cache_read : std_logic;
+signal cache_addr : std_logic_vector (31 downto 0);
+signal cache_write_data : std_logic_vector (31 downto 0);
+
 signal mem_read_data : std_logic_vector (31 downto 0);
 signal mem_waitrequest : std_logic;
 signal mem_write : std_logic;
 signal mem_read : std_logic;
 signal mem_addr : integer:=0;
 signal mem_write_data : std_logic_vector (31 downto 0);
+
+
 
 signal stall : std_logic;
 
@@ -88,16 +122,34 @@ port map(
 	ex_store=>ex_store,
 	wb_data=>wb_data,
 	wb_dest_reg=>wb_dest_reg,
-	mem_read_data=>mem_read_data,
-	mem_waitrequest=>mem_waitrequest,
-	mem_write=>mem_write,
-	mem_read=>mem_read,
-	mem_addr=>mem_addr,
-	mem_write_data=>mem_write_data,
+	mem_read_data=>cache_read_data,
+	mem_waitrequest=>cache_waitrequest,
+	mem_write=>cache_write,
+	mem_read=>cache_read,
+	mem_addr=>cache_addr,
+	mem_write_data=>cache_write_data,
 	stall=>stall
 );
 
-mem: data_memory
+cac: cache
+port map(
+	clock=>clk,	
+	reset=>reset,
+	s_writedata=>cache_write_data,
+	s_address=>cache_addr,
+	s_memwrite=>cache_write,
+	s_memread=>cache_read,
+	s_readdata=>cache_read_data,
+	s_waitrequest=>cache_waitrequest,
+	
+	m_address=>mem_addr,
+	m_memread=>mem_read,
+	m_readdata=>mem_read_data,
+	m_memwrite=>mem_write,
+	m_writedata=>mem_write_data,
+	m_waitrequest=>mem_waitrequest
+);
+mem: memory
 port map(
 	clock=>clk,
 	writedata=>mem_write_data,
@@ -107,7 +159,6 @@ port map(
 	readdata=>mem_read_data,
 	waitrequest=>mem_waitrequest
 );
-
 clk_process : process
 begin
   clk <= '0';
