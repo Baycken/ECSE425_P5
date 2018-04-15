76f5b635073648096a9d4616b1cf06ae18a6a086
Aidan Piwowar <apiwowar@users.noreply.github.com>
2018-04-14 23:00:49 -0400

    no success with hard coded instructions, revert

--- fetch.vhd
+++ fetch.vhd
@@ -1,79 +1,83 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
 
-entity fetch is
-generic(
-	ram_size : INTEGER := 32768
-);
-port(
-	clock : in std_logic;
-	reset : in std_logic;
-		if_id_test: out std_logic_vector(31 downto 0);
-
-	--communication with pc (getting and sending back the incremented one or the completely new pc)
-	addr : in std_logic_vector (31 downto 0);
-	--reply_back_pc : out std_logic_vector (31 downto 0);
-	--test
+ENTITY fetch IS
+	PORT (
+		reset : in std_logic;
+		clk : in std_logic;
 	
-	--just used to connect the instructionMemory in instructionFetch (so we can leave them floating in the instructionMemory)
-	--s_write : in std_logic; --not using
-	s_writedata : in std_logic_vector (31 downto 0); --not using 
-	
-	--communication with ID stage
-	hazard_detect : in std_logic:='0';
+		--execution stage communication
+		ex_is_new_pc: in std_logic;
+		ex_pc : in std_logic_vector(31 downto 0);
 
-	--communication with EX stage
-	ex_is_new_pc : in std_logic:='0';
-	ex_pc : in std_logic_vector(31 downto 0);
+		--decode stage communication
+		dc_pc : out std_logic_vector(31 downto 0);
+		dc_instr : out std_logic_vector(31 downto 0);
 
-	--communication with decode stage (**no need to write so comment)
-	instruction : out std_logic_vector(31 downto 0);
-	--instruction_read : out std_logic;
-	current_pc_to_dstage : out std_logic_vector(31 downto 0)
-);
+		--instruction memory communication
+		mem_read_data : in std_logic_vector (31 downto 0);
+		mem_waitrequest : in std_logic;
+		mem_write : out std_logic;
+		mem_read : out std_logic;
+		mem_addr : out integer RANGE 0 TO 8191;
+		mem_write_data : out std_logic_vector (31 downto 0);
 
+		--pipeline stall
+		hazard_detect : in std_logic
+	);
 end fetch;
 
-architecture arch of fetch is
+architecture fetch_arch of fetch is
 
---declarations
-signal pc_address : INTEGER RANGE 0 TO 1023:=to_integer(unsigned(addr));
-signal instruction_read_sig : std_logic:='0';
-signal wait_req : std_logic;
---signal s_write_sig : std_logic:='0';
-signal data : std_logic_vector(31 downto 0);
-signal program : std_logic_vector(31 downto 0):=x"20010003";
-
-component instr_mem
-    GENERIC(
-		ram_size : INTEGER := 32768;
-		mem_delay : time := 10 ns;
-		clock_period : time := 1 ns
-	);
-	PORT (
-		clock: IN STD_LOGIC;
-		writedata: IN STD_LOGIC_VECTOR (31 DOWNTO 0);
-		address: IN INTEGER RANGE 0 TO 1023;
-		memwrite: IN STD_LOGIC;
-		memread: IN STD_LOGIC;
-		readdata: OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
-		waitrequest: OUT STD_LOGIC
-	);
-end component;
+type state_type is (req_read, instr_read);
+signal state:state_type;
+signal temp_pc : std_logic_vector(31 downto 0);
 
 begin
 
-U1: component instr_mem port map (clock,s_writedata,pc_address,'0',instruction_read_sig,data,wait_req);
+mem_stage_process : process(clk,reset)
+variable pc : integer := 0;
+variable instruction: std_logic_vector(31 downto 0);
+
+begin
+
+if (reset = '1') then
+	temp_pc<=x"00000000";
+	state<=req_read;
+elsif (rising_edge(clk)) then
 	
-inst_get: process (clock)
-variable pc : integer:=0;
-begin
-	if (clock'event and clock='1') then 
-		instruction <= program;
-		if_id_test <= program;
-		current_pc_to_dstage<= std_logic_vector(to_unsigned(pc, current_pc_to_dstage'length));
-		pc:=pc+1;
+	if (ex_is_new_pc = '1') then
+		pc := to_integer(unsigned(ex_pc));
 	end if;
+
+
+	--read from isntruction memory
+	case state is
+		when req_read =>
+			mem_read <= '1';
+			mem_addr <= pc;
+			if rising_edge(mem_waitrequest) then
+				state<=instr_read;
+			end if;
+		when instr_read =>
+			mem_read<='0';
+			instruction := mem_read_data;
+			state<=req_read;
+			pc:=pc+1;
+		when others =>
+			state<=req_read;
+		end case;
+
+	if hazard_detect = '1' then--hazard
+		dc_pc <=x"00000000";
+		dc_instr <= x"00000000";
+		pc:=pc-1;
+	else--no hazard
+		dc_pc <= std_logic_vector(to_unsigned(pc, dc_pc'length));
+		dc_instr <= instruction;
+	end if;			
+		
+end if;
 end process;
-end arch;
+end fetch_arch;
